54d5084f7e0dfedce3044890bedb3a84a3ecf63d
bellllwon <bellwon95@gmail.com>
2025-03-04 13:53:20 +0900

    add: delete cards 기능 구현
    - api 연동
    - POST_CARDS naming 변경
    - delete cards test code 추가

--- src/card/api.ts
+++ src/card/api.ts
@@ -1,13 +1,38 @@
 import { postFetch } from "@/src/shared/util/data/fetcher"
 
-export const POST_CARDS = "/api/cards"
+export const CARDS = "/api/cards"
 
-export async function updateCards(data: CardForEditRequest[]): Promise<void> {
+export async function updateCards(
+  data: CardForEditRequest[],
+): Promise<Response> {
   const formData = new FormData()
   for (const [index, card] of data.entries()) {
     for (let [key, value] of Object.entries(card)) {
       formData.append(`requests[${index}].${key}`, value)
     }
   }
-  return postFetch(POST_CARDS, formData)
+  return postFetch(CARDS, formData)
+}
+
+export async function deleteCards(parameter: {
+  deckId: number
+  cardIds: string[]
+}): Promise<Response> {
+  return postFetch(CARDS, JSON.stringify(parameter), {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+  })
+}
+
+export async function mutateCards({
+  edited,
+  deleted,
+}: {
+  edited: CardForEditRequest[]
+  deleted: Parameters<typeof deleteCards>[0]
+}): Promise<void> {
+  await Promise.all([
+    edited.length ? updateCards(edited) : Promise.resolve(),
+    deleted.cardIds ? deleteCards(deleted) : Promise.resolve(),
+  ])
 }
