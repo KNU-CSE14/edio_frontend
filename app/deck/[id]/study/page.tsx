581971b127a3e2c17a2fff1d1db439f6d9dd07e1
bellllwon <bellwon95@gmail.com>
2025-04-23 15:36:07 +0900

    fix: server component queryOption staleTime 설정
    - clinet의 invalidateQuery는 server component에 적용되지 않으므로 staleTime 설정

--- app/deck/[id]/study/page.tsx
+++ app/deck/[id]/study/page.tsx
@@ -1,18 +1,5 @@
-import { getQueryClient } from "@/src/shared/get-query-client"
-import { getDeckDetail } from "@/src/deck/api"
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
 import SelectStudyType from "@/src/deck/study/SelectStudyType"
 
-export default async function page({ params }: { params: { id: number } }) {
-  const queryClient = getQueryClient()
-  try {
-    await queryClient.fetchQuery(getDeckDetail(params.id))
-  } catch (err) {
-    /// TODO: handle error
-  }
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <SelectStudyType />
-    </HydrationBoundary>
-  )
+export default async function page() {
+  return <SelectStudyType />
 }
