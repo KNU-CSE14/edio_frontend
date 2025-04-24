8b1ad080776e96f137123c4337cbad5eec93070c
bellllwon <bellwon95@gmail.com>
2025-04-24 15:52:54 +0900

    add: folder action button 구현

--- src/template/sidebar/AppSidebarContent.tsx
+++ src/template/sidebar/AppSidebarContent.tsx
@@ -1,6 +1,8 @@
 "use client"
 
-import SidebarDeckMenu from "@/src/template/sidebar/SidebarDeckMenu"
+import SidebarDeckMenu, {
+  DialogType,
+} from "@/src/template/sidebar/SidebarDeckMenu"
 import { Folder, getAllFolders } from "@/src/folder/api"
 import {
   Collapsible,
@@ -11,14 +13,23 @@ import { ScrollArea } from "@/src/shadcn/components/ui/scroll-area"
 import {
   SidebarContent,
   SidebarMenu,
+  SidebarMenuAction,
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarMenuSub,
 } from "@/src/shadcn/components/ui/sidebar"
 import SvgFolder from "@/src/shared/icons/SvgFolder"
 import { useQuery } from "@tanstack/react-query"
-import { ChevronRight } from "lucide-react"
 import useLocalStorage from "@/src/shared/hooks/useLocalStorage"
+import { ChevronRight, MoreHorizontal } from "lucide-react"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/src/shadcn/components/ui/dropdown-menu"
+import FolderDeleteDialog from "@/src/folder/FolderDeleteDialog"
+import { useRef } from "react"
 
 export default function AppSidebarContent() {
   const { data } = useQuery(getAllFolders())
@@ -27,9 +38,17 @@ export default function AppSidebarContent() {
   const [expandedFolders, setExpandedFolders] = useLocalStorage<{
     [key: string]: boolean
   }>("expandedFolders", {})
-
+  const dialogTriggerRef = useRef<Map<DialogType, HTMLElement | null>>(
+    new Map(),
+  )
+  const getRef = (id: DialogType) => dialogTriggerRef.current?.get(id)
+  const setRef = (id: DialogType) => (node: HTMLElement | null) => {
+    dialogTriggerRef.current.set(id, node)
+  }
   const generateFolderMenu = (folder: Folder) => {
     const isExpanded = !!expandedFolders[folder.id]
+    const hasSubFolder = !!folder?.subFolders.length
+    const hasDecks = !!folder?.decks.length
     return (
       <Collapsible open={isExpanded} key={`folder-${folder.id}`}>
         <CollapsibleTrigger
@@ -42,26 +61,64 @@ export default function AppSidebarContent() {
             }))
           }}
         >
-          <SidebarMenuButton>
-            <ChevronRight
-              className={`transition-transform ${isExpanded ? "rotate-90" : ""}`}
-            />
-            <SvgFolder />
-            <span>{folder?.name}</span>
-          </SidebarMenuButton>
+          <SidebarMenuItem className="truncate w-full max-w">
+            <SidebarMenuButton>
+              <ChevronRight
+                className={`transition-transform ${isExpanded ? "rotate-90" : ""}`}
+              />
+              <SvgFolder />
+              <span>{folder?.name}</span>
+            </SidebarMenuButton>
+            <DropdownMenu modal={false}>
+              <DropdownMenuTrigger asChild onClick={(e) => e.stopPropagation()}>
+                <SidebarMenuAction>
+                  <MoreHorizontal />
+                </SidebarMenuAction>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent side="right" align="start">
+                <DropdownMenuItem
+                  onClick={(e) => e.stopPropagation()}
+                  onSelect={() => {
+                    getRef("delete")?.click()
+                  }}
+                  asChild
+                >
+                  <span>Delete Deck</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onSelect={() => {
+                    ///TODO: getRef("edit")?.click()
+                  }}
+                  asChild
+                >
+                  <span>Edit Folder</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <FolderDeleteDialog folderId={folder.id}>
+              <div className="hidden" ref={setRef("delete")}></div>
+            </FolderDeleteDialog>
+          </SidebarMenuItem>
         </CollapsibleTrigger>
         <CollapsibleContent>
-          <SidebarMenuSub className="mr-0 pr-0">
-            {folder?.subFolders.map(generateFolderMenu)}
-          </SidebarMenuSub>
-          <SidebarMenuSub className="mr-0 pr-0">
-            {folder.decks.map((deck) => (
-              <SidebarDeckMenu
-                key={`deck-${deck.id}`}
-                deck={deck}
-              ></SidebarDeckMenu>
-            ))}
-          </SidebarMenuSub>
+          {hasSubFolder && (
+            <>
+              <div className="w-[1px]" />
+              <SidebarMenuSub className="mr-0 mb-0 pb-0 pr-0 translate-x-0">
+                {folder?.subFolders.map(generateFolderMenu)}
+              </SidebarMenuSub>
+            </>
+          )}
+          {hasDecks && (
+            <SidebarMenuSub className="mr-0 mb-0 pb-0 pr-0 translate-x-0">
+              {folder.decks.map((deck) => (
+                <SidebarDeckMenu
+                  key={`deck-${deck.id}`}
+                  deck={deck}
+                ></SidebarDeckMenu>
+              ))}
+            </SidebarMenuSub>
+          )}
         </CollapsibleContent>
       </Collapsible>
     )
@@ -69,16 +126,17 @@ export default function AppSidebarContent() {
 
   return (
     <SidebarContent>
-      <ScrollArea type="auto">
-        <SidebarMenu>
-          <SidebarMenuItem className="max-w-full px-1">
-            {folders?.map(generateFolderMenu)}
-            <SidebarMenu>
-              {decks?.map((deck) => (
-                <SidebarDeckMenu key={deck.id} deck={deck}></SidebarDeckMenu>
-              ))}
-            </SidebarMenu>
-          </SidebarMenuItem>
+      <ScrollArea
+        className="max-w-full w-full min-w-0 [&>[data-radix-scroll-area-viewport]>div]:!block"
+        type="auto"
+      >
+        <SidebarMenu className="max-w-full pr-2">
+          {folders?.map(generateFolderMenu)}
+          <SidebarMenu>
+            {decks?.map((deck) => (
+              <SidebarDeckMenu key={deck.id} deck={deck}></SidebarDeckMenu>
+            ))}
+          </SidebarMenu>
         </SidebarMenu>
       </ScrollArea>
     </SidebarContent>
